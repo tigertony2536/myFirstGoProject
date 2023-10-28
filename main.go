8bb273f29b693a5b2c38a5583582937e84dcaf2d
Thanapat <tigertony2536@gmail.com>
2023-10-28 21:11:41 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func changeString(text *string) {
+// 	*text = "Black Jack"
+// }
+
+func main() {
+	// var card string = "Aces of Spades"
+	// card := "Aces of Spades"
+	// changeString(&card)
+	// fmt.Println(&card)
+	test()
+}
