2e3e23d5d8e008098e800f1a2eee5a355c230528
Thanapat <tigertony2536@gmail.com>
2023-10-29 15:59:06 +0700

    add funtion newDeck for creating new deck of cards

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import "fmt"
-
-// import "fmt"
-
-// func changeString(text *string) {
-// 	*text = "Black Jack"
-// }
-
 func main() {
-	var card string = "Aces of Spades"
-	card := "Aces of Spades"
-	changeString(&card)
-	fmt.Println(&card)
-	test()
+	cards := newDeck()
+	cards.print()
 }
